07f0a7f0327f68016de3bcaeca8c874e950282c3
dfinke <finked@hotmail.com>
2021-11-21 09:35:01 -0500

    Add and run creatiom of charts

---
+++ CreateChartOfPriceOverTime.ps1
@@ -0,0 +1,29 @@
+$symbols = Get-Content coins.txt | Sort-Object
+
+$xlfileName = 'coins.xlsx'
+
+$worksheets = Get-ExcelFileSummary $xlfileName | Sort-Object worksheetname 
+
+$xlChartFileName = 'charts.xlsx'
+Remove-Item $xlChartFileName -ErrorAction SilentlyContinue
+
+$h = [ordered]@{}
+foreach ($worksheet in $worksheets) {
+    foreach ($symbol in $symbols) {
+        if (!$h.Contains($symbol)) {
+            $h["$symbol"] = @()
+        }
+        $h["$symbol"] += Import-Excel -Path $xlfileName -WorksheetName $worksheet.worksheetname | 
+        Where-Object { $_.id -eq $symbol } | 
+        Select-Object id, symbol, current_price, last_updated
+    }
+}
+
+$cd = New-ExcelChart -XRang last_updated -YRange current_price -NoLegend #-ChartType Line
+
+foreach ($entry in $h.GetEnumerator()) {
+    $cd.Title = "$($entry.Key)`nCurrent Price Over Time"
+    $entry.Value | Export-Excel $xlChartFileName -WorksheetName $entry.Key -AutoSize -AutoFilter -AutoNameRange -ExcelChartDefinition $cd
+}
+
+Invoke-Item $xlChartFileName

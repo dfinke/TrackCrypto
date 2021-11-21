c9f2047cb14f251eba19f4316dcf1e4d405f18d8
dfinke <finked@hotmail.com>
2021-11-21 10:38:18 -0500

    update date

--- CreateChartOfPriceOverTime.ps1
+++ CreateChartOfPriceOverTime.ps1
@@ -15,11 +15,11 @@ foreach ($worksheet in $worksheets) {
         }
         $h["$symbol"] += Import-Excel -Path $xlfileName -WorksheetName $worksheet.worksheetname | 
         Where-Object { $_.id -eq $symbol } | 
-        Select-Object id, symbol, current_price, last_updated
+        Select-Object id, symbol, current_price, @{n = 'last_updated'; e = { (Get-Date $_.last_updated).ToString("MM/dd/yyyy") } }
     }
 }
 
-$cd = New-ExcelChart -XRang last_updated -YRange current_price -NoLegend #-ChartType Line
+$cd = New-ExcelChart -XRange last_updated -YRange current_price -NoLegend #-ChartType Line
 
 foreach ($entry in $h.GetEnumerator()) {
     $cd.Title = "$($entry.Key)`nCurrent Price Over Time"

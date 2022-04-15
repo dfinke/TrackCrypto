958cada7101a5080e880241714b804859b78c75b
Doug Finke <finked@hotmail.com>
2022-04-15 19:48:00 -0400

    fix

--- readCrypto.py
+++ readCrypto.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 '_Data_ is updated **daily*** via a GitHub Action using PowerShell, Excel. Exporting to CSV.' 
-'At https://github.com/dfinke/TrackCrypt'
+'At https://github.com/dfinke/TrackCrypto'
 
 st.subheader('Crypto Data')
 

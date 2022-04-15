f30cb4507b75fe0c7e2a300c9a89bb6484c6933f
Doug Finke <finked@hotmail.com>
2022-04-15 19:35:22 -0400

    updated

--- readCrypto.py
+++ readCrypto.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+st.subheader('Crypto Data')
 
-for coin in ["solana", "bitcoin", "dogecoin", "litecoint", "ethereum", "cardano", "chainlink", "uniswap"]:
+for coin in ["solana", "bitcoin", "dogecoin", "litecoin", "ethereum", "cardano", "chainlink", "uniswap"]:
     if st.checkbox('Show ' + coin):
         st.subheader(coin)
         st.write(pd.read_csv(

0128655280f540bda4f00758dea2e3b2a6d95525
Doug Finke <finked@hotmail.com>
2022-04-16 09:37:39 -0400

    clean up

--- readCrypto.py
+++ readCrypto.py
@@ -17,21 +17,12 @@ def load_data(targetID):
 for coin in ["Solana", "Bitcoin", "Dogecoin", "Litecoin", "Ethereum", "Cardano", "Chainlink", "Uniswap"]:
     data = load_data(coin)
     st.subheader(coin)
-    # st.bar_chart(data)
-    # chart_data = pd.DataFrame(
-    #  np.random.randn(50, 3),
-    #  columns=["a", "b", "c"])
-
-    # st.bar_chart(chart_data)
 
     chart_data = pd.DataFrame(
         data.current_price
     )
-    # st.bar_chart(chart_data)
+
     st.line_chart(chart_data)
 
     if st.checkbox('show ' + coin + ' data'):
         st.write(data)
-        
-# hist_values = np.histogram(
-#     data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]

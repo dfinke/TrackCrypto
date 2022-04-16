1c2a1f6990df0d65a33e8a987e5cc0a73d616aee
Doug Finke <finked@hotmail.com>
2022-04-15 20:04:10 -0400

    update

--- readCrypto.py
+++ readCrypto.py
@@ -4,6 +4,7 @@ import pandas as pd
 """
 # Crypto Data
 Is updated ***daily*** via a:
+
 GitHub Action using PowerShell, Excel, and exporting to CSV: https://github.com/dfinke/TrackCrypto
 
 ### Show
